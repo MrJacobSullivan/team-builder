6d18b188f39dad62539d136a34bee620b4617e07
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-12 14:37:12 -0500

    wired up Form.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,43 @@ import Form from './components/Form'
 import TeamMemberList from './components/TeamMemberList'
 import './App.css'
 
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: '',
+}
+
 function App() {
   const [teamMembers, setTeamMembers] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [errorText, setErrorText] = useState('')
 
-  const handleChange = (name, value) => {}
+  const updateForm = (name, value) => {
+    setFormValues((prev) => ({ ...prev, [name]: value }))
+  }
+
+  const submitForm = () => {
+    const newMember = {
+      name: formValues.name.trim(),
+      email: formValues.email,
+      role: formValues.role,
+    }
+
+    if (!newMember.name || !newMember.email || !newMember.role) {
+      setErrorText(() => 'You need to enter a valid input for each field.')
+      return
+    }
+
+    setTeamMembers((prev) => [newMember, ...prev])
+    setFormValues(() => initialFormValues)
+    setErrorText(() => '')
+  }
 
   return (
     <div className='App'>
-      <Form handleChange={handleChange} />
+      <h1>Team Members</h1>
+
+      <Form values={formValues} update={updateForm} submit={submitForm} error={errorText} />
       <TeamMemberList teamMembers={teamMembers} />
     </div>
   )
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,36 @@
-import { useState } from 'react'
+export default function Form({ values, update, submit, error }) {
+  const onChange = (event) => {
+    const { name, value } = event.target
+    update(name, value)
+  }
 
-const initialFormValues = {
-  name: '',
-  email: '',
-  role: '',
-}
-
-export default function Form({ handleChange }) {
-  const [formValues, setFormValues] = useState(initialFormValues)
-
-  const handleSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault()
-
-    const { name, value } = event.target
-
-    handleChange(name, value)
+    submit(event)
   }
 
-  const handleFormChange = (event) => {
-    const { name, value } = event.target
+  return (
+    <form>
+      <label>
+        Name:
+        <input type='text' name='name' value={values.name} onChange={onChange} />
+      </label>
+      <label>
+        Email:
+        <input type='email' name='email' value={values.email} onChange={onChange} />
+      </label>
+      <label>
+        Role
+        <select value={values.role} name='role' onChange={onChange}>
+          <option value=''>-- Select a role --</option>
+          <option value='Student'>Student</option>
+          <option value='Team Lead'>Team Lead</option>
+          <option value='Instructor'>Instructor</option>
+          <option value='Alumni'>Alumni</option>
+        </select>
+      </label>
 
-    setFormValues((prev) => ({
-      ...prev,
-      [name]: value,
-    }))
-  }
-
-  return <form></form>
+      <button>submit</button>
+    </form>
+  )
 }
